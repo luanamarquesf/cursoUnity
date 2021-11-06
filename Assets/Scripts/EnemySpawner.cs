f45082b569ea21432f7cd482916eb357d86bab82
LuanaMarq <luana.ferreira223@gmail.com>
2021-11-06 15:01:07 -0300

    feat: add sprite and stand

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,14 +7,14 @@ public class EnemySpawner : MonoBehaviour
 
     public GameObject enemy;
 
-    GameObject player;
 
     List<GameObject> enemysAlive;
-
+    public int positionX = 26;
+    public int positionY = -3;
+    public int positionZ = 28;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
         enemysAlive = new List<GameObject>();
     }
 
@@ -34,8 +34,8 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        Vector3 enemyPosition = new Vector3(player.transform.position.x + Random.Range(-30, 30),
-            player.transform.position.y, player.transform.position.z + Random.Range(-30, 30));
+        Vector3 enemyPosition = new Vector3(26,
+            -3, 28);
 
         GameObject newEnemy = Instantiate(enemy, enemyPosition, Quaternion.identity);
 
